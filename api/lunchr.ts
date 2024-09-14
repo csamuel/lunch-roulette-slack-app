e90af0f158717a42ea99a594a7ab98e5fec94b6d
Chris Samuel <csamuel@gmail.com>
2024-09-14 17:09:51 -0500

    Formatting

--- api/lunchr.ts
+++ api/lunchr.ts
@@ -229,7 +229,7 @@ function toSlackBlocks(restaurant: Restaurant): Array<Block> {
       type: "section",
       text: {
         type: "mrkdwn",
-        text: `*<${url}|${name}>*\n_${categoryNames}_\n*<${menu_url}|Menu>*`,
+        text: `*<${url}|${name}>*\n_${categoryNames}_\n\n*<${menu_url}|Menu>*`,
       },
       accessory: {
         type: "image",
