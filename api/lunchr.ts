8d6278382a0492f98547e0c4614a0200774940f6
Chris Samuel <csamuel@gmail.com>
2024-09-14 14:07:14 -0500

    More fancy

--- api/lunchr.ts
+++ api/lunchr.ts
@@ -112,19 +112,158 @@ export default async (req: VercelRequest, res: VercelResponse) => {
         type: "section",
         text: {
           type: "mrkdwn",
-          text: "• Detective Chimp\n• Bouncing Boy\n• Aqualad",
+          text: "We found *205 Hotels* in New Orleans, LA from *12/14 to 12/17*",
+        },
+        accessory: {
+          type: "overflow",
+          options: [
+            {
+              text: {
+                type: "plain_text",
+                emoji: true,
+                text: "Option One",
+              },
+              value: "value-0",
+            },
+            {
+              text: {
+                type: "plain_text",
+                emoji: true,
+                text: "Option Two",
+              },
+              value: "value-1",
+            },
+            {
+              text: {
+                type: "plain_text",
+                emoji: true,
+                text: "Option Three",
+              },
+              value: "value-2",
+            },
+            {
+              text: {
+                type: "plain_text",
+                emoji: true,
+                text: "Option Four",
+              },
+              value: "value-3",
+            },
+          ],
         },
       },
       {
-        type: "image",
-        title: {
-          type: "plain_text",
-          text: "I love tacos",
-          emoji: true,
+        type: "divider",
+      },
+      {
+        type: "section",
+        text: {
+          type: "mrkdwn",
+          text: "*<fakeLink.toHotelPage.com|Windsor Court Hotel>*\n★★★★★\n$340 per night\nRated: 9.4 - Excellent",
         },
-        image_url:
-          "https://assets3.thrillist.com/v1/image/1682388/size/tl-horizontal_main.jpg",
-        alt_text: "delicious tacos",
+        accessory: {
+          type: "image",
+          image_url:
+            "https://api.slack.com/img/blocks/bkb_template_images/tripAgent_1.png",
+          alt_text: "Windsor Court Hotel thumbnail",
+        },
+      },
+      {
+        type: "context",
+        elements: [
+          {
+            type: "image",
+            image_url:
+              "https://api.slack.com/img/blocks/bkb_template_images/tripAgentLocationMarker.png",
+            alt_text: "Location Pin Icon",
+          },
+          {
+            type: "plain_text",
+            emoji: true,
+            text: "Location: Central Business District",
+          },
+        ],
+      },
+      {
+        type: "divider",
+      },
+      {
+        type: "section",
+        text: {
+          type: "mrkdwn",
+          text: "*<fakeLink.toHotelPage.com|The Ritz-Carlton New Orleans>*\n★★★★★\n$340 per night\nRated: 9.1 - Excellent",
+        },
+        accessory: {
+          type: "image",
+          image_url:
+            "https://api.slack.com/img/blocks/bkb_template_images/tripAgent_2.png",
+          alt_text: "Ritz-Carlton New Orleans thumbnail",
+        },
+      },
+      {
+        type: "context",
+        elements: [
+          {
+            type: "image",
+            image_url:
+              "https://api.slack.com/img/blocks/bkb_template_images/tripAgentLocationMarker.png",
+            alt_text: "Location Pin Icon",
+          },
+          {
+            type: "plain_text",
+            emoji: true,
+            text: "Location: French Quarter",
+          },
+        ],
+      },
+      {
+        type: "divider",
+      },
+      {
+        type: "section",
+        text: {
+          type: "mrkdwn",
+          text: "*<fakeLink.toHotelPage.com|Omni Royal Orleans Hotel>*\n★★★★★\n$419 per night\nRated: 8.8 - Excellent",
+        },
+        accessory: {
+          type: "image",
+          image_url:
+            "https://api.slack.com/img/blocks/bkb_template_images/tripAgent_3.png",
+          alt_text: "Omni Royal Orleans Hotel thumbnail",
+        },
+      },
+      {
+        type: "context",
+        elements: [
+          {
+            type: "image",
+            image_url:
+              "https://api.slack.com/img/blocks/bkb_template_images/tripAgentLocationMarker.png",
+            alt_text: "Location Pin Icon",
+          },
+          {
+            type: "plain_text",
+            emoji: true,
+            text: "Location: French Quarter",
+          },
+        ],
+      },
+      {
+        type: "divider",
+      },
+      {
+        type: "actions",
+        elements: [
+          {
+            type: "button",
+            text: {
+              type: "plain_text",
+              emoji: true,
+              text: "Next 2 Results",
+            },
+            value: "click_me_123",
+          },
+        ],
       },
     ];
 
