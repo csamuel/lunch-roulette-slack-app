1b74e607b5f2f78ab09ab829e9e4c608bea935fd
Chris Samuel <csamuel@gmail.com>
2026-03-21 20:50:52 -0500

    psuedo monorepo

--- api/types/restaurant/index.ts
+++ api/types/restaurant/index.ts
@@ -1,17 +1,1 @@
-interface Restaurant {
-  id: string;
-  name: string;
-  url: string;
-  image_url: string;
-  distance: number;
-  price: string;
-  display_address: string[];
-  rating: number;
-  location: { display_address: string[] };
-  categories: { title: string }[];
-  attributes?: {
-    menu_url?: string;
-  };
-}
-
-export type { Restaurant };
+export type { Restaurant } from '@lunch-roulette/types';
